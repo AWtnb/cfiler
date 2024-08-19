31aa20398a4c85ab77ffbd890defba5faf18393f
awtnb <arikaebanataw@gmail.com>
2024-08-19 12:11:01 +0900

    update

--- config.py
+++ config.py
@@ -141,8 +141,8 @@ def configure(window: MainWindow) -> None:
             "C-Down": window.command_CursorDownSelectedOrBookmark,
             "C-J": window.command_CursorDownSelectedOrBookmark,
             "C-C": window.command_SetClipboard_Fullpath,
-            "S-SemiColon": window.command_MoveSeparatorLeft,
-            "SemiColon": window.command_MoveSeparatorRight,
+            "8": window.command_MoveSeparatorLeft,
+            "9": window.command_MoveSeparatorRight,
             "A-S": window.command_SetSorter,
         }
     )
