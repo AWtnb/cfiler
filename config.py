fbd54310f01a6014a30820e1262f88cb09130eb0
awtnb <arikaebanataw@gmail.com>
2024-09-01 10:37:36 +0900

    fix

--- config.py
+++ config.py
@@ -3,3 +3,15 @@ from setting import setup
 
 def configure(window) -> None:
     setup.configure(window)
+
+
+def configure_ListWindow(window) -> None:
+    setup.configure_ListWindow(window)
+
+
+def configure_TextViewer(window) -> None:
+    setup.configure_TextViewer(window)
+
+
+def configure_ImageViewer(window) -> None:
+    setup.configure_ImageViewer(window)
