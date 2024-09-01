bd75e8ba38d7e337a8c156f035708612cf35f958
awtnb <arikaebanataw@gmail.com>
2024-09-01 11:59:20 +0900

    update

--- config.py
+++ config.py
@@ -8,15 +8,12 @@ def configure(window) -> None:
 
 
 def configure_ListWindow(window) -> None:
-    importlib.reload(setup)
     setup.configure_ListWindow(window)
 
 
 def configure_TextViewer(window) -> None:
-    importlib.reload(setup)
     setup.configure_TextViewer(window)
 
 
 def configure_ImageViewer(window) -> None:
-    importlib.reload(setup)
     setup.configure_ImageViewer(window)
