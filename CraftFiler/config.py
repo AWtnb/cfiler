b22e7280a61148a83eca4fc6381eeaf2987abaf1
awtnb <arikaebanataw@gmail.com>
2025-06-12 11:12:03 +0900

    default search all

--- CraftFiler/config.py
+++ CraftFiler/config.py
@@ -1347,8 +1347,8 @@ def configure(window: MainWindow) -> None:
 
     def setup_zyw():
         for params, key in {
-            (False, False): "Z",
-            (False, True): "A-Z",
+            (False, False): "S-Z",
+            (False, True): "Z",
             (True, False): "S-F",
             (True, True): "C-F",
         }.items():
