542dd5a479b42aa67a1c235c842de85802aea60e
awtnb <arikaebanataw@gmail.com>
2025-11-13 09:49:52 +0900

    update

--- CraftFiler/config.py
+++ CraftFiler/config.py
@@ -1024,19 +1024,14 @@ def configure(window: MainWindow) -> None:
         border_width = 1
         window_width = window.width() - border_width
 
-        left_width = window_width // 2
         if window.focus == MainWindow.FOCUS_LEFT:
-            if min_width <= window.left_window_width:
-                left_width = min_width
-            else:
-                left_width = min(min_width, window_width)
+            if window.left_window_width < min_width:
+                window.left_window_width = min(min_width, window_width)
         else:
-            if min_width <= window_width - window.left_window_width:
-                left_width = window_width - min_width
-            else:
-                left_width = max(window_width - min_width, 0)
+            right_width = window_width - window.left_window_width
+            if right_width < min_width:
+                window.left_window_width = max(window_width - min_width, 0)
 
-        window.left_window_width = left_width
         window.updateThemePosSize()
         pane.repaint(PaintOption.Upper)
 
