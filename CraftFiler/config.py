602be48c0214a786374534d2c9c8eb2c425282bc
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-04-27 10:30:58 +0900

    add key

--- CraftFiler/config.py
+++ CraftFiler/config.py
@@ -4661,6 +4661,7 @@ def configure_TextViewer(window: ckit.TextWindow) -> None:
         pyauto.shellExecute(None, smooth_csv_path, str(path), "")
 
     window.keymap["C-S-Enter"] = open_with_smooth_csv
+    window.keymap["A-S-C"] = open_with_smooth_csv
 
     def open_original(_) -> None:
         pane = window.main_window.activePane()
