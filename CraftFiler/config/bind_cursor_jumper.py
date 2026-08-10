f8e99ce05aa95120baa6ca262d4eab5050e77c78
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-08-10 14:05:12 +0900

    fix

--- CraftFiler/config/bind_cursor_jumper.py
+++ CraftFiler/config/bind_cursor_jumper.py
@@ -8,28 +8,26 @@ def setup(window) -> None:
     cursor_jumper.setup(window)
     keybinder.setup(window)
 
+    for (by_prefix, selecting), key in {
+        (True, False): "A-J",
+        (True, True): "S-A-J",
+        (False, False): "C-J",
+        (False, True): "S-C-J",
+    }.items():
 
-for (by_prefix, selecting), key in {
-    (True, False): "A-J",
-    (True, True): "S-A-J",
-    (False, False): "C-J",
-    (False, True): "S-C-J",
-}.items():
+        def _jump_down(b=by_prefix, s=selecting) -> None:
+            cursor_jumper.CursorJumper(b).down(s)
 
-    def _jump_down(b=by_prefix, s=selecting) -> None:
-        cursor_jumper.CursorJumper(b).down(s)
+        keybinder.bind(_jump_down, key)
 
-    keybinder.bind(_jump_down, key)
+    for (by_prefix, selecting), key in {
+        (True, False): "A-K",
+        (True, True): "S-A-K",
+        (False, False): "C-K",
+        (False, True): "S-C-K",
+    }.items():
 
+        def _jump_up(b=by_prefix, s=selecting) -> None:
+            cursor_jumper.CursorJumper(b).up(s)
 
-for (by_prefix, selecting), key in {
-    (True, False): "A-K",
-    (True, True): "S-A-K",
-    (False, False): "C-K",
-    (False, True): "S-C-K",
-}.items():
-
-    def _jump_up(b=by_prefix, s=selecting) -> None:
-        cursor_jumper.CursorJumper(b).up(s)
-
-    keybinder.bind(_jump_up, key)
+        keybinder.bind(_jump_up, key)
